5194082cf99a2a5893ce37dcbaed2179e7f34b0c
Ayush singla <143862828+Ayushsingla1@users.noreply.github.com>
2024-10-05 19:40:09 +0530

    just

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
-import axios from "axios";
 
-async function getUserDetails() {
-  // await new Promise((r)=>setTimeout(r, 5000));
-  const response = await axios.get("http://localhost:3000/api/user")
-	return response.data;
-}
 
 export default async function Home() {
-  const userDetails = await getUserDetails();
   return (
     <div className="bg-black w-screen h-screen text-center text-white">
       hello
-      <br/>
-      {userDetails.email}
-      <br/>
-      {userDetails.name}
     </div>
   );
 }
